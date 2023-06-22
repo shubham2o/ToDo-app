6b5f77dd34b783797b560651900c7d01a247767a
shubham2o <shubhamsharma.ha@gmail.com>
2023-06-22 16:57:03 +0530

    Added localStorage and code spell checker + code completed

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-const form =  document.getElementById("form");
+const form = document.getElementById("form");
 const input = document.getElementById("input");
-const todos = document.getElementById("todos");
+const todosUl = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+if (todos) {
+    todos.forEach(todo => addToDo(todo))
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    addToDo();
+});
 
-    const todoText = input.value;
+function addToDo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
 
     if (todoText) {
         const todoEl = document.createElement('li');
+
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
+
         todoEl.innerHTML = todoText;
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         });
 
-        todos.appendChild(todoEl);
-
+        todosUl.appendChild(todoEl);
         input.value = "";
+        updateLS();
     }
-});
+}
+
+function updateLS() {
+    const notesEl = document.querySelectorAll('li');
+    const notes = [];
+
+    notesEl.forEach((noteEl) => {
+        notes.push({
+            Task: noteEl.innerText, Completed: noteEl.classList.contains('completed')
+        });
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
